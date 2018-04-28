1f9247d2071cfe7534879571969f7445ad4e6bd5
bennyzhu <benny@jesquare.com>
2018-04-28 14:33:26 +0800

    Fixed bug

--- src/game_stats.py
+++ src/game_stats.py
@@ -5,6 +5,6 @@ class GameStats():
         self.game_active = False
         self.high_score = 0
     def reset_stats(self):
-        self.ship_left = self.ai_settings.ship_limit
+        self.ships_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
